0ab3b21e4f6daab1a02ba5dfab9cf326171f1a41
Antoexpo <antoexpo999@gmail.com>
2025-09-15 17:23:55 +0200

    Delete apps directory

--- apps/api/src/config.js
+++
@@ -1,3 +0,0 @@
-import compPlan from '../../packages/config/comp_plan.json' assert { type: 'json' };
-
-export const CONFIG = compPlan;
--- apps/api/src/prisma.js
+++
@@ -1,4 +0,0 @@
-import { PrismaClient } from '@prisma/client';
-
-const url = process.env.DATABASE_URL || 'file:./dev.db';
-export const prisma = new PrismaClient({ datasources: { db: { url } } });
